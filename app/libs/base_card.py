15fd0698e4ec8143ac2c0df51b581ef0968e577b
wuwent52620 <1043792865@qq.com>
2022-01-19 00:56:47 +0800

    also init

--- app/libs/base_card.py
+++ app/libs/base_card.py
@@ -1,7 +1,8 @@
 import json
 import os
-from collections import deque
+import random
 
+from collections import deque
 from app.config import kind_list, root_path
 
 card_gen = {key: deque(range(1, 13)) for key in kind_list}
@@ -26,13 +27,21 @@ def gen_card(kind, num=None):
 
 class Blood(object):
     def __set__(self, instance, value):
-        print(instance, value)
         if value < 0:
             setattr(instance, 'alive', False)
         setattr(instance, '_blood', value)
 
     def __get__(self, instance, owner):
         return getattr(instance, '_blood')
+
+
+class CardMeta(type):
+    def __new__(cls, clsname, clsbase, clsdict):
+        file_path = os.path.join(root_path, 'card', f'{clsname.lower()}.json')
+        with open(file_path, 'rb') as fl:
+            base_info = json.load(fl)
+            clsdict.update(base_info=base_info)
+        return super().__new__(cls, clsname, clsbase, clsdict)
 
 
 class CardMixin(object):
@@ -42,26 +51,25 @@ class CardMixin(object):
             func(target)
 
 
-class RoleCard(CardMixin):
-    __slots__ = []
-    blood = Blood()
+class RoleCard(CardMixin, metaclass=CardMeta):
 
     def __init__(self, **kwargs):
         [setattr(self, key, value) for key, value in kwargs.items()]
         self.alive = True
 
+    @classmethod
+    def create(cls):
+        cls.__slots__ = tuple(cls.base_info['cards'][0].keys())
+        cards = deque()
+        for single in cls.base_info['cards']:
+            cards.append(cls(**single))
+        random.shuffle(cards)
+        return cards
 
-class CardMeta(type):
-    def __new__(cls, clsname, clsbase, clsdict):
-        file_path = os.path.join(root_path, 'card', f'{clsname.lower()}.json')
-        with open(file_path, 'rb') as fl:
-            base_info = json.load(fl)
-            clsdict.update(_base_info=base_info)
-        return super().__new__(cls, clsname, clsbase, clsdict)
+    def __repr__(self):
+        return f"{self.name}--{self.info}"
 
 
-class BaseCard(metaclass=CardMeta):
-    __slots__ = ['name', 'kind', 'number', 'image', 'info']
-
-
-print(BaseCard.__subclasses__())
+if __name__ == '__main__':
+    res = RoleCard.create()
+    print(res)
